46252120ac2fbc7ce6d573bc592c42cef3e5d2f1
manu <manuthomasmt123@gmail.com>
2024-10-11 17:59:33 +0530

    first problem solved

--- array/secondlargest.py
+++ array/secondlargest.py
@@ -1,1 +1,13 @@
-print("starting dsa")
+a=[2,7,4,9,1]
+m=99
+sm=99
+for i in range(len(a)):
+    # for j in range(i+1,len(a)):
+        if a[i]<m:
+            t=m
+            m=a[i]
+            sm=t
+        elif a[i]>m and a[i]<sm:
+            sm=a[i]
+print(sm)
+        
